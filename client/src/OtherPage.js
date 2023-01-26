d39da4806ed607386482a0a774306a8b6f36c5a7
Marek Pieczykolan <105322938+Demenciusz@users.noreply.github.com>
2023-01-26 23:36:17 +0100

    in

---
+++ client/src/OtherPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      multi-fig
+      <br></br>
+      Marek Pieczykolan
+      <br></br>
+      I2S 2.3
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
